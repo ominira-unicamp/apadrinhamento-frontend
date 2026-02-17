d4332bb50046881e05a9f59f20c549613d5d865e
Gvinfinity <gabrielsoares@berry.dev.br>
2026-02-16 21:03:27 -0300

    feat: Add terms of service

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Routes, Outlet, Navigate, Route } from "react-router-dom
 import { useAuth } from "./hooks/useAuth";
 import { useEffect } from "react";
 
-import { SignupPage, LoginPage, LandPage, HomePage, ApprovalPage, StatsPage, GodparentingPage, ResultPage, AdminDashboardPage, AllUsersPage, ResetPasswordPage, ForgotPasswordPage } from "./pages";
+import { SignupPage, LoginPage, LandPage, HomePage, ApprovalPage, StatsPage, GodparentingPage, ResultPage, AdminDashboardPage, AllUsersPage, ResetPasswordPage, ForgotPasswordPage, TermsOfServicePage } from "./pages";
+import { TermsOfServiceFooter } from "./components";
 
 const PrivateRoutes = () => {
     const auth = useAuth();
@@ -35,27 +36,30 @@ export const AppRoutes = () => {
 
     return (
         <BrowserRouter>
-            <Routes>
-                <Route element={<PrivateRoutes/>}>
-                {/* <Route element={<Outlet/>}> */}
-                    <Route path="*" element={<Navigate to='/'/>}/>
-                    <Route path="/dashboard" element={<HomePage/>}/>
-                    <Route element={<AdminRoute/>}>
-                        <Route path="/admin" element={<AdminDashboardPage/>}/>
-                        <Route path="/admin/approval" element={<ApprovalPage/>}/>
-                        <Route path="/admin/users" element={<AllUsersPage/>}/>
-                        <Route path="/admin/stats" element={<StatsPage/>}/>
-                        <Route path="/admin/godparenting" element={<GodparentingPage/>}/>
+            <>
+                <Routes>
+                    <Route element={<PrivateRoutes/>}>
+                        <Route path="*" element={<Navigate to='/'/>}/>
+                        <Route path="/dashboard" element={<HomePage/>}/>
+                        <Route element={<AdminRoute/>}>
+                            <Route path="/admin" element={<AdminDashboardPage/>}/>
+                            <Route path="/admin/approval" element={<ApprovalPage/>}/>
+                            <Route path="/admin/users" element={<AllUsersPage/>}/>
+                            <Route path="/admin/stats" element={<StatsPage/>}/>
+                            <Route path="/admin/godparenting" element={<GodparentingPage/>}/>
+                        </Route>
+                        <Route path="/results" element={<ResultPage/>}/>
                     </Route>
-                    <Route path="/results" element={<ResultPage/>}/>
-                </Route>
 
-                <Route path="/" element={<LandPage />}/>
-                <Route path="/login" element={<LoginPage/>}/>
-                <Route path="/signup" element={<SignupPage/>}/>
-                <Route path="/forgot-password" element={<ForgotPasswordPage/>}/>
-                <Route path="/reset-password" element={<ResetPasswordPage/>}/>
-            </Routes>
+                    <Route path="/" element={<LandPage />}/>
+                    <Route path="/login" element={<LoginPage/>}/>
+                    <Route path="/signup" element={<SignupPage/>}/>
+                    <Route path="/forgot-password" element={<ForgotPasswordPage/>}/>
+                    <Route path="/reset-password" element={<ResetPasswordPage/>}/>
+                    <Route path="/termos" element={<TermsOfServicePage/>}/>
+                </Routes>
+                <TermsOfServiceFooter />
+            </>
         </BrowserRouter>
     )
 }
