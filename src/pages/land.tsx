26f7bee0e84271d8edc8258a09d557695f8d930d
Gvinfinity <gabrielsoares@berry.dev.br>
2026-02-20 12:56:20 -0300

    feat: Update text

--- src/pages/land.tsx
+++ src/pages/land.tsx
@@ -8,8 +8,8 @@ export const LandPage = () => {
     return (
         <div className="w-full h-full flex flex-col items-center gap-3 p-2 pt-5 bg-zinc-800 overflow-y-scroll">
             <img src={Logo} className="w-1/2 lg:w-1/6 md:w-1/4 h-fit aspect-square" />
-            <h1 className="text-4xl text-center font-extrabold text-cyan-200">Bem-vinde ao Sistema de Apadrinhamento da Computação</h1>
-            <p className="mt-8 text-xl max-w-3xl text-white text-center">Esse é um sistema criado pela Comissão de Trote da Computação 2026, nele vocês serão separados em pares ou trios de veteranes e bixes para conhecer como funciona a Unicamp e tirar todas as dúvidas!</p>
+            <h1 className="text-4xl text-center font-extrabold text-cyan-200">Bem-vinde ao Sistema de Apadrinhamento da INSERIR CURSO</h1>
+            <p className="mt-8 text-xl max-w-3xl text-white text-center">Esse é um sistema criado pela Ominira, nele vocês serão separados em grupos de ingressantes e veteranes para conhecer como funciona a Unicamp e tirar todas as dúvidas!</p>
             <button className="mt-8 bg-amber-600 text-white text-xl font-bold py-2 px-4 rounded-lg cursor-pointer" onClick={() => navigate("/login")}>Começar</button>
         </div>
     )
